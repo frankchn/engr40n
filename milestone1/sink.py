dcfcb4b9498ff9dd8caac5347ebde6b014801b57
Frank Chen <frankchn@gmail.com>
2013-04-10 23:01:54 -0700

    source and sink

--- milestone1/sink.py
+++ milestone1/sink.py
@@ -24,23 +24,53 @@ class Sink:
         
         # If its an image, save it as "rcd-image.png"
         # If its a text, just print out the text
+
+        rcd_payload = None
+        header = recd_bits[:32]
+
+        srctype, payload_length = self.read_header(header)
+        payload = recd_bits[32:(32 + payload_length * 8)]
+
+        if srctype == 'text':
+            rcd_payload = self.bits2text(payload)
+        elif srctype == 'image':
+            self.image_from_bits(payload, '')
         
         # Return the received payload for comparison purposes
         return rcd_payload
 
     def bits2text(self, bits):
-        # Convert the received payload to text (string)
+        string_bits = [str(i) for i in bits]
+        text = ''
+        
+        for i in range(len(string_bits)):
+            if i % 8 > 0:
+                continue
+            val = chr(int(''.join(string_bits[i:i+8]), 2))
+            text += val
+
         return  text
 
     def image_from_bits(self, bits,filename):
-        # Convert the received payload to an image and save it
-        # No return value required .
+        v = self.bits2text(bits)
+        with open("rcd-image.png", "w+") as f:
+            f.write(v)
         pass 
 
     def read_header(self, header_bits): 
         # Given the header bits, compute the payload length
         # and source type (compatible with get_header on source)
- 
+
+        string_bits = [str(i) for i in header_bits]
+
+        payload_length = int(''.join(string_bits[2:]), 2)
+        payload_type = int(''.join(string_bits[:2]), 2)
+        srctype = {
+            0: 'text',
+            1: 'image',
+            2: 'monotone'
+        }[payload_type]
+
         print '\tRecd header: ', header_bits
         print '\tLength from header: ', payload_length
         print '\tSource type: ', srctype
--- milestone1/source.py
+++ milestone1/source.py
@@ -4,37 +4,65 @@ import Image
 from graphs import *
 import binascii
 import random
-
-
+import os
+import itertools
 
 class Source:
     def __init__(self, monotone, filename=None):
         # The initialization procedure of source object
         self.monotone = monotone
         self.fname = filename
-        print 'Source: '
+        print 'Source: Filename: ' + filename + ', Monotone: ' + str(monotone)
+
+    # header format is 32 bits long
+    # bits one and two are:
+    # 0,0 - TXT
+    # 0,1 - PNG
+    # 1,1 - Monotone
+    # The next 30 bits are the range. The maximum is 2^30-1 (or 1 byte less than 1 GB)
 
     def process(self):
-            # Form the databits, from the filename 
-            if self.fname is not None:
-                if self.fname.endswith('.png') or self.fname.endswith('.PNG'):
-                    # Its an image
-                else:           
-                    # Assume it's text                    
-            else:               
-                # Send monotone (the payload is all 1s for 
-                # monotone bits)   
-            return payload, databits
+        # Form the databits, from the filename 
+        if self.fname is not None:
+            if self.fname.endswith('.png') or self.fname.endswith('.PNG'):
+                databits = self.bits_from_image(self.fname)
+                content_type = 'image'
+                content_len = self.filesize(self.fname)
+            else:
+                databits = self.text2bits(self.fname)  
+                content_type = 'text'
+                content_len = self.filesize(self.fname)
+        else:               
+            databits = [ 1 for x in range(self.monotone) ]
+            content_type = 'monotone'
+            content_len = self.monotone
+
+        header = self.get_header(content_len, content_type)
+        payload = header + databits
+
+        return payload, databits
+
+    def filesize(self, filename):
+        return os.path.getsize(filename)
+
+    def num2bits(self, n):
+        return [ int(z) for z in bin(n)[2:].zfill(30) ][:30]
 
     def text2bits(self, filename):
-        # Given a text file, convert to bits
-        return bits
+        with open(filename, "r") as myfile:
+            data = myfile.read()
+        return [int(j) for j in list(itertools.chain(*[list('%08d' % int(bin(ord(i))[2:])) for i in data]))]
 
     def bits_from_image(self, filename):
-        # Given an image, convert to bits
-        return bits
+        with open(filename, "r") as myfile:
+            data = myfile.read()
+        return [int(j) for j in list(itertools.chain(*[list('%08d' % int(bin(ord(i))[2:])) for i in data]))]
 
     def get_header(self, payload_length, srctype): 
         # Given the payload length and the type of source 
         # (image, text, monotone), form the header
-        return header
+        return {
+                  'text':     [0,0] + self.num2bits(payload_length),
+                  'image':    [0,1] + self.num2bits(payload_length),
+                  'monotone': [1,0] + self.num2bits(payload_length)
+               }[srctype]
