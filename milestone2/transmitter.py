4be12e7ce4b597630264610d503fef98a38dde14
Frank Chen <frankchn@gmail.com>
2013-04-21 15:26:22 -0700

    some code for the transmitter

--- milestone2/transmitter.py
+++ milestone2/transmitter.py
@@ -10,6 +10,7 @@ class Transmitter:
         self.spb = spb
         self.silence = silence
         print 'Transmitter: '
+    
     def add_preamble(self, databits):
         '''
         Prepend the array of source bits with silence bits and preamble bits
@@ -18,7 +19,11 @@ class Transmitter:
         The output should be the concatenation of arrays of
             [silence bits], [preamble bits], and [databits]
         '''
-        # fill in your implementation
+        
+        silence_bits = [ 0 for x in range(self.silence) ]
+        preamble_bits = [1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1]
+        databits_with_preamble = silence_bits + preamble_bits + databits
+
         return databits_with_preamble
 
 
@@ -28,9 +33,16 @@ class Transmitter:
         Sample values for bit '1', '0' should be [one], 0 respectively.
         Output should be an array of samples.
         '''
-        # fill in your implemenation
+        samples = numpy.array([])
+
+        for i in range(len(databits_with_preamble)):
+            for j in range(self.spb):
+                if databits_with_preamble == 1:
+                    samples = numpy.append(samples, 'one')
+                else:
+                    samples = numpy.append(samples, 0);
+
         return samples
-        
 
     def modulate(self, samples):
         '''
