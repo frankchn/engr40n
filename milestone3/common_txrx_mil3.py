8b7dd653660bf7b3e7fcae43f56b8ba7f30ec4c6
Frank Chen <frankchn@gmail.com>
2013-05-26 16:23:22 -0700

    milestone 3 carrier mod/demod done

--- milestone3/common_txrx_mil3.py
+++ milestone3/common_txrx_mil3.py
@@ -4,25 +4,50 @@ import operator
 
 # Methods common to both the transmitter and receiver
 def modulate(fc, samplerate, samples):
-  '''
-  A modulator that multiplies samples with a local carrier 
-  of frequency fc, sampled at samplerate
-  '''
-  return 0
+  print "Modulating with carrier frequency " + str(fc)
+  return samples * numpy.cos(carrier_signal(fc, samplerate, len(samples)))
+
+def carrier_signal(fc, samplerate, sz):
+  return numpy.arange(0, sz) * fc * 2 * math.pi / samplerate
 
 def demodulate(fc, samplerate, samples):
-  '''
-  A demodulator that performs quadrature demodulation
-  '''
-  return 0
+  print "Demodulating with carrier frequency " + str(fc)
+  l = samples * numpy.cos(carrier_signal(fc, samplerate, len(samples)))
+  Q = samples * -1 * numpy.sin(carrier_signal(fc, samplerate, len(samples)))
+
+  print "Performing low pass filter"
+  lp = lpfilter(l, math.pi * fc / samplerate)
+  lQ = lpfilter(Q, math.pi * fc / samplerate)
+
+  out = []
+  for n in xrange(0, len(samples)):
+    out.append(math.sqrt(math.pow(lp[n], 2) + math.pow(lQ[n], 2)))
+
+  return out
 
 def lpfilter(samples_in, omega_cut):
   '''
   A low-pass filter of frequency omega_cut.
   '''
-  # set the filter unit sample response
   L = 50
+  h = [] # shifted by 50
+
+  for n in xrange(-L, L):
+    if n == 0:
+      h.append(omega_cut / math.pi)
+    else:
+      h.append(math.sin(omega_cut * n) / (math.pi * n))
   
+  out = []
+  for n in xrange(0, len(samples_in)):
+    accu = 0
+    for x in xrange(-L, L):
+      ai = n + x - L
+      if(ai < 0 or ai >= n):
+        continue
+      accu += samples_in[ai] * h[x - L]
+    out.append(accu)
+
   # compute the demodulated samples
-  return 0
+  return out
 
--- milestone3/receiver_mil3.py
+++ milestone3/receiver_mil3.py
@@ -15,19 +15,48 @@ def detect_threshold(demod_samples):
         # the zeroes and ones. We have decided to avoid that degree of
         # complexity in audiocom (for the time being, anyway).
 
+  print "Detecting threshold with 2-means"
+
 	# initialization
   center1 = min(demod_samples)
   center2 = max(demod_samples) 
 
   # insert code to implement 2-means clustering 	
+  c1f = 0
+  c1c = 0
+  c2f = 0
+  c2c = 0
+
+  for x in xrange(0, len(demod_samples)):
+    if(math.fabs(center1 - demod_samples[x]) > math.fabs(center2 - demod_samples[x])):
+      c2f += demod_samples[x]
+      c2c += 1
+    else:
+      c1f += demod_samples[x]
+      c1c += 1
+
+  center1 = c1f / c1c
+  center2 = c2f / c2c
+
+  one = 0.0
+  zero = 0.0
+
+  if(center1 > center2):
+    one = center1
+    zero = center2
+  else:
+    one = center2
+    zero = center1
  
   # insert code to associate the higher of the two centers 
   # with one and the lower with zero
   
   print "Threshold for 1:"
   print one
-  print " Threshold for 0:"
+  print "Threshold for 0:"
   print zero
+
+  thresh = (one - zero) / 2
 
   # insert code to compute thresh
   return one, zero, thresh
